0df401aee464f0905a0029b405dccf652dfbbef0
bahakz <bahakz@gmail.com>
2015-06-15 15:17:00 +0600

    added detailed view for images

---
+++ Day 4 Photo Bombers App/DetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Bakytzhan Baizhikenov on 6/15/15.
+//  Copyright (c) 2015 Bakytzhan Baizhikenov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (nonatomic) NSDictionary *photo; 
+
+@end
