91bba23b8f387eb004c30c064002ecbbde078d0e
bahakz <bahakz@gmail.com>
2015-06-15 16:03:55 +0600

    animations to the presenting view added

--- Day 4 Photo Bombers App/PresentDetailTransition.m
+++ Day 4 Photo Bombers App/PresentDetailTransition.m
@@ -13,7 +13,7 @@
 
 -(NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext
 {
-    return 0.3;
+    return 0.1;
 }
 
 -(void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
@@ -31,7 +31,7 @@
     detailVC.view.frame = frame;
     [containerView addSubview:detailVC.view];
     
-    [UIView animateWithDuration:0.3 animations:^{
+    [UIView animateWithDuration:0.1 animations:^{
         detailVC.view.alpha = 1.0;
     } completion:^(BOOL finished) {
         [transitionContext completeTransition:YES];
