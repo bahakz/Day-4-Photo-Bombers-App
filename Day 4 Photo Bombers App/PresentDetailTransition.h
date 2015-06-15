613f1be5e206600dff0e3c97187ff8116c0b50ff
bahakz <bahakz@gmail.com>
2015-06-15 15:52:53 +0600

    Fade in and Fade out transitions added to detail view controller

---
+++ Day 4 Photo Bombers App/PresentDetailTransition.h
@@ -0,0 +1,14 @@
+//
+//  PresentDetailTransition.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Bakytzhan Baizhikenov on 6/15/15.
+//  Copyright (c) 2015 Bakytzhan Baizhikenov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface PresentDetailTransition : NSObject <UIViewControllerAnimatedTransitioning>
+
+@end
---
+++ Day 4 Photo Bombers App/dismissDetailTransition.m
@@ -0,0 +1,31 @@
+//
+//  dismissDetailTransition.m
+//  Day 4 Photo Bombers App
+//
+//  Created by Bakytzhan Baizhikenov on 6/15/15.
+//  Copyright (c) 2015 Bakytzhan Baizhikenov. All rights reserved.
+//
+
+#import "dismissDetailTransition.h"
+
+@implementation dismissDetailTransition
+
+
+-(NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    return 0.3;
+}
+
+-(void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+    UIViewController *detailVC = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        detailVC.view.alpha = 0.0;
+    } completion:^(BOOL finished) {
+        [detailVC.view removeFromSuperview];
+        [transitionContext completeTransition:YES];
+    }];
+}
+
+@end
